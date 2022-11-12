b9ffd84f1a006ce5d986130d7124db72e4c1fec5
Luke <nethaven@hotmail.com>
2022-11-11 22:32:43 -0500

    Multiple questions load, randomness maybe broken

--- Scripts/BedGame.cs
+++ Scripts/BedGame.cs
@@ -10,76 +10,129 @@ public partial class BedGame : CanvasLayer
 	Label RightLabel;
 	Label QuestionLabel;
 	Node2D Player;
+	Control PlayerStartPosition;
+
 	Question Question;
 
+	Random RandomQuestionNumber = new Random();
 
+	//bool MovementTweenActive = false;
+	public enum MiniGameState {PLAYING, SELECTING_ANSWER, ANSWER_SELECTED, CORRECT_ANSWER_SELECTED, INCORRECT_ANSWER_SELECTED};
+	private MiniGameState CurrentState = MiniGameState.PLAYING;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		LeftLabel = GetNode<Label>("./LeftLabel");
 		RightLabel = GetNode<Label>("./RightLabel");
+		QuestionLabel = GetNode<Label>("./QuestionLabel");
 
 		// Player node is NOT a child of the CanvasLayer, so get the parent first (essenitally, end up with the "sibling" node)
 		Player = GetNode<Node2D>("../Player");
+		PlayerStartPosition = GetNode<Control>("./StartPosition2D");
 
-		Question = DatabaseHandler.QuestionCollection.FindOne(x => x.MiniGame == "Bed");
-
-		// Set the question text
-		QuestionLabel = GetNode<Label>("./QuestionLabel");
-		QuestionLabel.Text = Question.QuestionText;
-
-		// Set the answers
-		// Randomize loop
-		Random r = new Random();
-		foreach (int i in Enumerable.Range(0, Question.Answers.Count).OrderBy(x => r.Next()))
-		{
-			if (LeftLabel.Text == "#")
-				LeftLabel.Text = Question.Answers[i].Key;
-			else if (RightLabel.Text == "#")
-				RightLabel.Text = Question.Answers[i].Key;
-		}
-
+		LoadQuestion();
 	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		var AnswerText = "";
-
 		if (Input.IsActionJustPressed("ui_left"))
 		{
-			MoveToAnswer(LeftLabel);
-			CheckAnswer(LeftLabel.Text);
+			if (CurrentState == MiniGameState.PLAYING)
+			{
+				MoveToAnswer(LeftLabel);
+				CheckAnswer(LeftLabel.Text);
+			}
 		}
 		else if (Input.IsActionJustPressed("ui_right"))
 		{
-			MoveToAnswer(RightLabel);
-			CheckAnswer(RightLabel.Text);
+			if (CurrentState == MiniGameState.PLAYING)
+			{
+				MoveToAnswer(RightLabel);
+				CheckAnswer(RightLabel.Text);
+			}
+		}
+
+	}
+
+	
+
+
+
+	private void LoadQuestion()
+	{
+		Player.GetNode<Sprite>("Sprite").Position = PlayerStartPosition.RectPosition;
+
+		Question = DatabaseHandler.QuestionCollection.Find(x => x.MiniGame == "Bed" && x.Difficulty == 1).OrderBy(x => RandomQuestionNumber.Next()).First();
+
+		// Set the question text
+		
+		QuestionLabel.Text = Question.QuestionText;
+
+		// Set the answers
+		// Randomize loop
+		Random RandomLoopNumber = new Random();
+		foreach (int i in Enumerable.Range(0, Question.Answers.Count).OrderBy(x => RandomLoopNumber.Next()))
+		{
+			// GD.Print(i);
+
+			if (i == 0)
+				LeftLabel.Text = Question.Answers[i].Key;
+			else if (i == 1)
+				RightLabel.Text = Question.Answers[i].Key;
 		}
 
 	}
 
 
-
-	private void MoveToAnswer(Label Target)
+	
+	private async void MoveToAnswer(Label Target)
 	{
 		// RectPosition returns the top-left corner, we desire the center
 		var TargetPosition = Target.RectPosition + Target.RectScale * Target.RectSize / 2.0f;
 
-		var Tween = CreateTween();
-		Tween.TweenProperty(Player.GetNode("Sprite"), "position", TargetPosition, .5f);
+		var Twn = CreateTween();
+		CurrentState = MiniGameState.SELECTING_ANSWER;
+
+		Twn.TweenProperty(Player.GetNode("Sprite"), "position", TargetPosition, .5f);
+		await ToSignal(Twn, "finished");
+
+		CurrentState = MiniGameState.ANSWER_SELECTED;
+		
 	}
 
-	private void CheckAnswer(string AnswerText)
+
+
+	private async void CheckAnswer(string AnswerText)
 	{
+		
 		var Answer = Question.Answers.Where(x => x.Key == AnswerText).FirstOrDefault();
 
 		if (Answer.Value == true)
+		{
+			CurrentState = MiniGameState.CORRECT_ANSWER_SELECTED;
 			GD.Print("Good job, WRONG answer!");
+
+			var DelayTimer = GetTree().CreateTimer(2.5f);
+			await ToSignal(DelayTimer, "timeout");
+
+			CurrentState = MiniGameState.PLAYING;
+			LoadQuestion();
+		}
 		else if (Answer.Value == false)
+		{
+			CurrentState = MiniGameState.INCORRECT_ANSWER_SELECTED;
 			GD.Print("Incorrect, RIGHT answer!");
+
+			var DelayTimer = GetTree().CreateTimer(2.5f);
+			await ToSignal(DelayTimer, "timeout");
+
+			CurrentState = MiniGameState.PLAYING;
+			LoadQuestion();
+		}
+		
 
 	}
 }
--- Scripts/Database/DefaultQuestions.cs
+++ Scripts/Database/DefaultQuestions.cs
@@ -5,19 +5,37 @@ public partial class DefaultQuestions : Node
 {
     public override void _Ready()
     {
-        var question = new Question 
+        List<Question> QuestionList = new List<Question>()
         {
-            MiniGame = "Bed",
-            Difficulty = 1,
-            IsInPlay = true,
-            QuestionText = "2 + 2",
-            Answers = new List<KeyValuePair<string, bool>>
+            new Question 
             {
-                new KeyValuePair<string, bool>("4", false),
-                new KeyValuePair<string, bool>("5", true)
+                MiniGame = "Bed",
+                Difficulty = 1,
+                IsInPlay = true,
+                QuestionText = "2 + 2",
+                Answers = new List<KeyValuePair<string, bool>>
+                {
+                    new KeyValuePair<string, bool>("4", false),
+                    new KeyValuePair<string, bool>("5", true)
+                }
+            },
+
+            new Question
+            {
+                MiniGame = "Bed",
+                Difficulty = 1,
+                IsInPlay = true,
+                QuestionText = "Pointed Ears",
+                Answers = new List<KeyValuePair<string, bool>>
+                {
+                    new KeyValuePair<string, bool>("Cat", false),
+                    new KeyValuePair<string, bool>("Mouse", true)
+                }
             }
         };
 
-        DatabaseHandler.QuestionCollection.Insert(question);
+
+        foreach(var Quest in QuestionList)
+            DatabaseHandler.QuestionCollection.Insert(Quest);
     }
 }
